abcb183a811eb8806134b9142eb587e58511fd39
Marius Tews <uk069544@student.uni-kassel.de>
2022-04-28 12:51:29 +0200

    feat: login fxml and lobby fxml
    again minor changes also in LobbyController and LoginController for compatibility with fxml files

--- src/main/java/com/aviumauctores/pioneers/controller/LobbyController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/LobbyController.java
@@ -1,9 +1,28 @@
 package com.aviumauctores.pioneers.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class LobbyController implements Controller {
+
+    @FXML public Label gameLabel;
+
+    @FXML public ListView gameListView;
+
+    @FXML public Label playerLabel;
+
+    @FXML public ListView playerListView;
+
+    @FXML public Button createGameButton;
+
+    @FXML public Button chatButton;
+
+    @FXML public Button quitButton;
+
 
     public void init(){
 
