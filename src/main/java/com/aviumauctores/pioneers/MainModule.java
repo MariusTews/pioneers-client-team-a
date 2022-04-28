3863b92312867ecae73d81510391f651a899d386
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-04-28 12:37:29 +0200

    feat: Add dagger dependencies

--- src/main/java/com/aviumauctores/pioneers/MainModule.java
+++ src/main/java/com/aviumauctores/pioneers/MainModule.java
@@ -1,4 +1,32 @@
 package com.aviumauctores.pioneers;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import javax.inject.Singleton;
+
+@Module
 public class MainModule {
+    @Provides
+    @Singleton
+    static ObjectMapper mapper() {
+        return new ObjectMapper()
+                .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
+    }
+
+    @Provides
+    @Singleton
+    static Retrofit retrofit(ObjectMapper mapper) {
+        return new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(JacksonConverterFactory.create(mapper))
+                .build();
+    }
 }
