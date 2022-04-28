83ff3564202676dc9934d6c5ff3c9761de972aea
NSchintze <uk078202@student.uni-kassel.de>
2022-04-28 16:41:55 +0200

    feat: created ChatScreen FXML

--- src/main/java/com/aviumauctores/pioneers/controller/ChatController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/ChatController.java
@@ -1,8 +1,19 @@
 package com.aviumauctores.pioneers.controller;
 
 import javafx.scene.Parent;
+import javafx.scene.control.*;
 
 public class ChatController implements Controller {
+
+    public TextField chatTextField;
+    public Button sendButton;
+    public ListView onlinePlayerList;
+    public Button leaveButton;
+    public ScrollBar chatScrollBar;
+    public Label hintLabel;
+    public Label onlinePlayerLabel;
+    public Tab allTab;
+    public TabPane chatTabPane;
 
     public void init(){
 
