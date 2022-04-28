c28b95624720f2e0228590c4aabc8cb87ef50f80
jannis1607 <jannis.umbach@gmail.com>
2022-04-28 15:02:15 +0200

    feat: add registerScreen.fxml

--- src/main/java/com/aviumauctores/pioneers/controller/CreateAnAccountController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/CreateAnAccountController.java
@@ -1,8 +1,17 @@
 package com.aviumauctores.pioneers.controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CreateAnAccountController implements Controller {
+
+    public TextField textfieldUsername;
+    public Button accountErstellenButton;
+    public Button verlassenButton;
+    public Button anzeigenButton;
+    public TextField textfieldPasswort;
 
     public void init(){
 
@@ -14,5 +23,16 @@ public class CreateAnAccountController implements Controller {
 
     public Parent render(){
         return null;
+    }
+
+    public void anzeigen(ActionEvent event) {
+
+    }
+
+    public void verlassen(ActionEvent event) {
+    }
+
+    public void accountErstellen(ActionEvent event) {
+
     }
 }
