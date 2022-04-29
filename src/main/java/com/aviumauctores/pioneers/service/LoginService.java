ff7d97074e1561e85af93b57fab928eba672d436
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-04-29 18:22:34 +0200

    feat: Save access token in LoginService.tokenStorage

--- src/main/java/com/aviumauctores/pioneers/service/LoginService.java
+++ src/main/java/com/aviumauctores/pioneers/service/LoginService.java
@@ -10,13 +10,16 @@ import javax.inject.Inject;
 public class LoginService {
 
     private final AuthenticationApiService authenticationApiService;
-    @Inject
-    public LoginService(AuthenticationApiService authenticationApiService){
+    private final TokenStorage tokenStorage;
 
+    @Inject
+    public LoginService(AuthenticationApiService authenticationApiService, TokenStorage tokenStorage){
         this.authenticationApiService = authenticationApiService;
+        this.tokenStorage = tokenStorage;
     }
 
     public Observable<LoginResult> login(String username, String password){
-        return authenticationApiService.login(new LoginDto(username, password));
+        return authenticationApiService.login(new LoginDto(username, password))
+                .doOnNext(result -> tokenStorage.setToken(result.accessToken()));
     }
 }
