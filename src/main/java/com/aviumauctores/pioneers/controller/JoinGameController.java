0a5e042966f8e12b84391dfcc1c55f3b51076586
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-05-05 17:47:39 +0200

    feat: Create joinGameScreen.fxml and update JoinGameController

--- src/main/java/com/aviumauctores/pioneers/controller/JoinGameController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/JoinGameController.java
@@ -1,8 +1,19 @@
 package com.aviumauctores.pioneers.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class JoinGameController implements Controller {
+
+    @FXML public Label gameNameLabel;
+    @FXML public PasswordField passwordTextField;
+    @FXML public Button showPasswordButton;
+    @FXML public Button joinGameButton;
+    @FXML public Button leaveButton;
 
     public void init(){
 
@@ -14,5 +25,17 @@ public class JoinGameController implements Controller {
 
     public Parent render(){
         return null;
+    }
+
+    public void showPassword(ActionEvent actionEvent) {
+
+    }
+
+    public void joinGame(ActionEvent actionEvent) {
+
+    }
+
+    public void quit(ActionEvent actionEvent) {
+
     }
 }
