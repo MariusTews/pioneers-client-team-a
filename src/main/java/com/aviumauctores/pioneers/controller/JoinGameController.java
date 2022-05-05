197a5fc66d0bd3cefe8dc1617ee846693e394b80
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-05-05 18:41:16 +0200

    feat: Show password when button clicked

--- src/main/java/com/aviumauctores/pioneers/controller/JoinGameController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/JoinGameController.java
@@ -1,16 +1,22 @@
 package com.aviumauctores.pioneers.controller;
 
+import com.aviumauctores.pioneers.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class JoinGameController implements Controller {
 
     @FXML public Label gameNameLabel;
     @FXML public PasswordField passwordTextField;
+    public TextField showPasswordTextField;
     @FXML public Button showPasswordButton;
     @FXML public Button joinGameButton;
     @FXML public Button leaveButton;
@@ -24,11 +30,26 @@ public class JoinGameController implements Controller {
     }
 
     public Parent render(){
-        return null;
+        final FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/joinGameScreen.fxml"));
+        loader.setControllerFactory(c -> this);
+        final Parent parent;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        passwordTextField.textProperty().bindBidirectional(showPasswordTextField.textProperty());
+        showPasswordTextField.setManaged(false);
+        return parent;
     }
 
     public void showPassword(ActionEvent actionEvent) {
-
+        boolean passwordShowed = showPasswordTextField.isVisible();
+        passwordTextField.setVisible(passwordShowed);
+        passwordTextField.setManaged(passwordShowed);
+        showPasswordTextField.setVisible(!passwordShowed);
+        showPasswordTextField.setManaged(!passwordShowed);
     }
 
     public void joinGame(ActionEvent actionEvent) {
