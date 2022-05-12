0e10bcacb3128b3980badde7f64b76da0268bbc7
MikeK01 <mkarabet87@gmail.com>
2022-05-12 17:18:03 +0200

    feat: Created new Branch

--- src/main/java/com/aviumauctores/pioneers/controller/ChatController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/ChatController.java
@@ -87,20 +87,9 @@ public class ChatController implements Controller {
                         groupService.updateGroup(ALLCHAT_ID, usersIdList).subscribe();
                     }
                 }));
+
         // listen for incoming messages and show them as a Label
-        disposable.add(eventListener.listen("groups.*.messages.*.*", Message.class)
-                .observeOn(FX_SCHEDULER)
-                .subscribe(event -> {
-                    Label msgLabel = createMessageLabel(event.data());
-                    if (event.event().endsWith(".created")) {
-                        ((VBox)((ScrollPane)this.allTab.getContent()).getContent()).getChildren()
-                                .add(msgLabel);
-                    }
-                    else if (event.event().endsWith(".deleted")) {
-                        ((VBox)((ScrollPane)this.allTab.getContent()).getContent()).getChildren()
-                                .remove(msgLabel);
-                    }
-                }));
+
     }
 
     public void destroy(){
