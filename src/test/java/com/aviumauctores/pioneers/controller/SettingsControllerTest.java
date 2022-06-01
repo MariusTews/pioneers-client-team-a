a8784f42ce0b05f6829b68e3d38ec3859d4e80f4
NSchintze <uk078202@student.uni-kassel.de>
2022-06-01 13:19:23 +0200

    test: change Username

---
+++ src/test/java/com/aviumauctores/pioneers/controller/SettingsControllerTest.java
@@ -0,0 +1,61 @@
+package com.aviumauctores.pioneers.controller;
+
+import com.aviumauctores.pioneers.App;
+import com.aviumauctores.pioneers.dto.users.UpdateUserDto;
+import com.aviumauctores.pioneers.model.User;
+import com.aviumauctores.pioneers.service.UserService;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SettingsControllerTest extends ApplicationTest {
+
+
+    @Mock
+    UserService userService;
+
+    @Spy
+    ResourceBundle bundle = ResourceBundle.getBundle("com/aviumauctores/pioneers/lang", Locale.ROOT);
+
+    @InjectMocks
+    SettingsController settingsController;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        User user = new User("123", "Hans", "online", null, null);
+        when(userService.getUserByID(any())).thenReturn(Observable.just(user));
+        new App(settingsController).start(stage);
+    }
+
+    @Test
+    void changeUserName() {
+        //User user = new User("123", "Hans", "online", null, null);
+        //when(userService.getUserByID(any())).thenReturn(Observable.just(user));
+        //when(userService.getCurrentUserID()).thenReturn("123");
+        when(userService.updateUser(any(), new UpdateUserDto(any(), null, null, null, null)))
+                .thenReturn(Observable.empty());
+
+        clickOn("#changeNameButton");
+        clickOn("#newParameterField");
+        write("Peter");
+        clickOn("#acceptChangesButton");
+
+        verify(userService).updateUser("123", new UpdateUserDto("Peter", null, null, null, null));
+    }
+
+}
