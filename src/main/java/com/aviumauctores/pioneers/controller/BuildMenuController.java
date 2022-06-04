a5aef6102cc68dba116c8226ba6f084f933ade59
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-06-04 21:47:47 +0200

    feat: Create FXML and controller for build menu

---
+++ src/main/java/com/aviumauctores/pioneers/controller/BuildMenuController.java
@@ -0,0 +1,20 @@
+package com.aviumauctores.pioneers.controller;
+
+import javafx.scene.Parent;
+
+public class BuildMenuController implements Controller {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy(boolean closed) {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
