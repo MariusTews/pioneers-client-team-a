c5627f098325459b62396f5f220b2e0c8ae7956f
MikeK01 <mkarabet87@gmail.com>
2022-06-11 19:49:13 +0200

    fix : CI push

--- src/test/java/com/aviumauctores/pioneers/controller/InGameControllerTest.java
+++ src/test/java/com/aviumauctores/pioneers/controller/InGameControllerTest.java
@@ -30,6 +30,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+
 @ExtendWith(MockitoExtension.class)
 public class InGameControllerTest extends ApplicationTest {
     @Mock
