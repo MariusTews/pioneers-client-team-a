e9c9be29bc19e13f205c5acbbd30374725b9eb3e
Florian <flo.schadow@gmx.net>
2022-06-20 21:37:15 +0200

    feat:GameServerBasicStructure - Test for UpdatePlayerDto and RobDto

--- src/test/java/com/aviumauctores/pioneers/dto/DtoTest.java
+++ src/test/java/com/aviumauctores/pioneers/dto/DtoTest.java
@@ -1,6 +1,9 @@
 package com.aviumauctores.pioneers.dto;
 
 import com.aviumauctores.pioneers.dto.auth.LoginDto;
+import com.aviumauctores.pioneers.dto.games.UpdateGameDto;
+import com.aviumauctores.pioneers.dto.players.UpdatePlayerDto;
+import com.aviumauctores.pioneers.dto.rob.RobDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +17,22 @@ class DtoTest {
         );
         Assertions.assertEquals(loginDto.name(), "Struppi");
         Assertions.assertEquals(loginDto.password(), "12345678");
+
+        UpdatePlayerDto updatePlayerDto=new UpdatePlayerDto(true);
+        Assertions.assertEquals(updatePlayerDto.active(),true);
+        UpdatePlayerDto updatePlayerDto1=new UpdatePlayerDto(false);
+        Assertions.assertEquals(updatePlayerDto1.active(),false);
+
+        RobDto robDto= new RobDto(1,2,3,"target");
+        Assertions.assertEquals(robDto.x(),1);
+        Assertions.assertEquals(robDto.y(),2);
+        Assertions.assertEquals(robDto.z(),3);
+        Assertions.assertEquals(robDto.target(),"target");
+
     }
+
+
+
+
+
 }
