a790779f12e34862dab9552680c5b6d26d6b5d07
mariustews <mariust2001@gmail.com>
2022-07-10 11:32:33 +0200

    refactor: zoom and drag utils

---
+++ src/main/java/com/aviumauctores/pioneers/util/DragContext.java
@@ -0,0 +1,12 @@
+package com.aviumauctores.pioneers.util;
+
+/**
+ * Mouse drag context used for scene.
+ */
+class DragContext {
+    double mouseAnchorX;
+    double mouseAnchorY;
+
+    double translateAnchorX;
+    double translateAnchorY;
+}
