0d7f07fe182a825ec7277a96690283a08ea08f66
jannis1607 <jannis.umbach@gmail.com>
2022-07-16 16:41:29 +0200

    fix: changed fx:id to id

--- src/main/java/com/aviumauctores/pioneers/controller/LobbyController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/LobbyController.java
@@ -17,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
