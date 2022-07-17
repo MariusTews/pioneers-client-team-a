c4866de8e7804b7f83da1ba59ccbf3e052d793e9
LucasScheerer <lucas@scheerer-nieheim.de>
2022-07-17 14:46:36 +0200

    fix: resolved an error that originated from merging

--- src/main/java/com/aviumauctores/pioneers/controller/InGameController.java
+++ src/main/java/com/aviumauctores/pioneers/controller/InGameController.java
@@ -26,6 +26,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
